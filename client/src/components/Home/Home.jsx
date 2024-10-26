6f10a5bcce8f47d105ef692c06ac4394a39a4a05
aadarsh1503 <aadarshchauhan35@gmail.com>
2024-10-26 12:55:10 +0530

    commit

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import HeroSection from '../Hero/Hero'
 import Hero1 from '../Hero/Hero1'
-import Slide from '../Slider/SLIDER.JSX'
 import WhyChooseUs from '../WhyChooseUs/WhyChooseUs'
 import Testimonals from '../Testimonals/Testimonals'
 import ContactForm from '../Contactus/Contactus'
+import Slide from '../Slider/Slider'
 
 const Home = () => {
   return (
