da199c28ea725bd31a3dbab7cb5dc339e6fcb021
aadarsh1503 <aadarshchauhan35@gmail.com>
2024-10-26 22:11:32 +0530

    first

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,8 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './',
-})
+  build: {
+    outDir: 'build', // Specify your desired output directory
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor'; // Separate vendor chunk
+          }
+          if (id.includes('src/components')) {
+            return 'components'; // Separate chunk for components
+          }
+        }
+      }
+    },
+    chunkSizeWarningLimit: 500, // Adjust chunk size warning limit if needed
+  }
+});
